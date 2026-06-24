1740e35dbf11789ce2610bdd58e4188497073b03
Sujal Gurung <151057335+gurungsujall@users.noreply.github.com>
2026-06-24 11:30:21 +0530

    Update fibonacci.cpp

--- day2/fibonacci.cpp
+++ day2/fibonacci.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fibbo(int a){
+    if(a==0){
+        return 0;
+    }
+    if(a==1){
+        return 1;
+    }
+    return fibbo(a-1)+fibbo(a-2);
+    
+}
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	cout<<fibbo(a);
+	return 0;
+
+}
 
