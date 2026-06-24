8dfc2df9e893cdd9c4a7b419a595ce5c2c0107ac
Sujal Gurung <151057335+gurungsujall@users.noreply.github.com>
2026-06-24 10:06:48 +0530

    Implement palindrome check using recursion

--- day1/palindrome.cpp
+++ day1/palindrome.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+bool palindrome(string s, int start, int end) {
+    if (start >= end)
+        return true;
+
+    if (s[start] != s[end])
+        return false;
+
+    return palindrome(s, start + 1, end - 1);
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (palindrome(s, 0, s.length() - 1))
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
