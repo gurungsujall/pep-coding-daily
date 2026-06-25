cb666dcc578496cf1f8d25ae60b35da1b6ef6a7f
Sujal Gurung <151057335+gurungsujall@users.noreply.github.com>
2026-06-25 07:04:10 +0530

    Implement iterative Fibonacci function in C++

--- day2/fibonacci(iterative).cpp
+++ day2/fibonacci(iterative).cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fibbo(int a){
+    int first=0,second=1;
+    for(int i=1;i<=a;i++){
+        int next=first+second;
+        first= second;
+        second=next;
+
+        
+        
+    }
+    return first;
+    
+}
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	cout<<fibbo(a);
+	return 0;
+	
+
+}
 
