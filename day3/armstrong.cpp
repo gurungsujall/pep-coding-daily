48a63bb4e6e8dd929e22b5a09070988d43021756
Sujal Gurung <151057335+gurungsujall@users.noreply.github.com>
2026-06-25 11:48:56 +0530

    Add Armstrong number check function
    Implement function to check if a number is an Armstrong number.

--- day3/armstrong.cpp
+++ day3/armstrong.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+bool arm(int a){
+    int sum=0;
+    int original=153;
+    while(a>0){
+        int digit=a%10;
+        sum=sum+pow(digit,3);
+        a=a/10;
+    }
+    if(sum==original){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+int main()
+{
+    cout<<arm(153);
+    return 0;
+}
