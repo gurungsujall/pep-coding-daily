a09f6cf94c2a8f7467f8a086d2d47e36f5cc8494
Sujal Gurung <151057335+gurungsujall@users.noreply.github.com>
2026-07-21 14:58:15 +0530

    Time: 0 ms (100%), Space: 15.2 MB (44.29%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto i:nums){
+            mp[i]++;
+        }
+        priority_queue<
+    pair<int,int>,
+    vector<pair<int,int>>,
+    greater<pair<int,int>>
+> p;
+        vector<int> ans;
+        for(auto i:mp){
+            p.push({i.second,-i.first});
+        }
+        while(!p.empty()){
+            auto f=p.top();
+            p.pop();
+            for(int i=0;i<f.first;i++){
+                ans.push_back(-f.second);
+            }
+        }
+        return ans;
+
+
+    }
+};
