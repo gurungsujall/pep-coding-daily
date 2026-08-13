e33c303a0f89faae8a88af45fc063c15cc4f9200
Sujal Gurung <151057335+gurungsujall@users.noreply.github.com>
2026-08-13 21:43:19 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -7,15 +7,13 @@ public:
                 swap(nums[l],nums[m]);
                 l++;
                 m++;
-            }else if(nums[m]==1)
-                {
-                    m++;
-                }
-            else{
-                swap(nums[h],nums[m]);
+            }
+            else if(nums[m]==1){
+                m++;
+            }else{
+                swap(nums[m],nums[h]);
                 h--;
             }
         }
-      
     }
 };
